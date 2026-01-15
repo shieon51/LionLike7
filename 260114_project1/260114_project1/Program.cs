8db4c32bbbcb27aaf3c533bf6be9d4f4fe24c621
shieon51 <slimo0501@naver.com>
2026-01-15 09:18:23 +0900

    9일차 수업

---
+++ 260114_project1/260114_project1/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _260114_project1
+{
+    //클래스끼리 통신
+    class Player
+    {
+        public int hp;
+        public int att;
+
+        public void SetHp(int hp)
+        {
+            this.hp = hp;
+        }
+
+        public int GetHp()
+        {
+            return hp;
+        }
+
+        public void SetAtt(int att)
+        {
+            this.att = att;
+        }
+
+        public int GetAtt()
+        {
+            return att;
+        }
+
+        public void Render()
+        {
+            Console.WriteLine("플레이어");
+            Console.WriteLine("체력 : " + hp);
+            Console.WriteLine("공격력 : " + att);
+        }
+
+    }
+    class Monster
+    {
+        public int hp;
+        public int att;
+
+        public void SetHp(int hp)
+        {
+            this.hp = hp;
+        }
+
+        public int GetHp()
+        {
+            return hp;
+        }
+
+        public void SetAtt(int att)
+        {
+            this.att = att;
+        }
+
+        public int GetAtt()
+        {
+            return att;
+        }
+
+        public void Render()
+        {
+            Console.WriteLine("몬스터");
+            Console.WriteLine("체력 : " + hp);
+            Console.WriteLine("공격력 : " + att);
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //플레이어객체
+            Player player = new Player();
+            player.SetAtt(10);
+            player.SetHp(100);
+            player.Render();
+
+            //몬스터객체
+            Monster monster = new Monster();
+            monster.SetAtt(5);
+            monster.SetHp(100);
+            monster.Render();
+
+            //monster.hp = monster.hp - player.att;
+            //player.hp = player.hp - monster.att;
+
+            //player.Render();
+            //monster.Render();
+
+            //플레이어가 몬스터 때리기   
+            monster.SetHp(monster.GetHp() - player.GetAtt());
+            //몬스터가 플레이어 때리기
+            player.SetHp(player.GetHp() - monster.GetAtt());
+
+        }
+    }
+}
---
+++ 260114_project4/260114_project4/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _260114_project4
+{
+    public class Character
+    {
+        public virtual void Render()
+        {
+            Console.WriteLine("캐릭터");
+        }
+    }
+
+    public class Warrior : Character
+    {
+        public override void Render()
+        {
+            Console.WriteLine("워리어");
+        }
+    }
+
+    public class Mage : Warrior
+    {
+        public override void Render()
+        {
+            Console.WriteLine("마법사");
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Character character = new Character();
+            // character.Render(); //캐릭터
+            //Character character = new Warrior(); //업캐스팅   부모 <- 자식메모리 참조
+            //character.Render();
+            //Character character = new Mage();
+            //character.Render();//마법사
+
+            //Warrior warrior = new Mage();
+            //warrior.Render();
+
+            // Mage mage = new Mage();
+            // mage.Render();
+
+            Character character = new Warrior();  //업캐스팅
+
+            Warrior warrior = (Warrior)character; //다운캐스팅
+
+            warrior.Render();
+
+        }
+    }
+}
